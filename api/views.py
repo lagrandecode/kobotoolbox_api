876b69bafdb04802cfa167a11a193ad3f99422ca
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-05 11:01:41 +0100

    "commit"

--- api/views.py
+++ api/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    import requests
+    import json
+    header = {
+        "Authorization": "Token 4605bc6fa1bc107993ea297fd0d32ef670398ed3"
+    }
+    
+
+
+    kobo = requests.get('https://kobo.humanitarianresponse.info/api/v2/assets/azpMY3AfSzcU97PNQV9eb8.json',headers=header)
+    api = json.loads(kobo.content)
+    context = {
+        'api' : api
+    }
+    return render(request, 'home.html', context)
