60c5fb516603fb655ee8e7bd8e7d4fdd913020fa
lagrandecode <seunogunmolufirst1@gmail.com>
2022-09-10 10:52:51 +0100

    "commit"

--- api/views.py
+++ api/views.py
@@ -14,7 +14,8 @@ def home(request):
     kobo = requests.get('https://kc.humanitarianresponse.info/api/v1/data/1180169.json',headers=header)
     api = json.loads(kobo.content)
 
-    
+    for element in api:
+        print(element)
 
     context = {
         'api' : api,
