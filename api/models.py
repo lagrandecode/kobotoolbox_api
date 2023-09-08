be6706bb4434fc13fde22abab56ad8035e6b173a
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-08 20:14:11 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -8,6 +8,28 @@ class Kobo(models.Model):
     name = models.CharField(max_length=300)
 
 
-
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=40)
+    price = models.PositiveIntegerField()
+    description=models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    STATUS =(
+        ('Pending','Pending'),
+        ('Order Confirmed','Order Confirmed'),
+        ('Out for Delivery','Out for Delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
+    email = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=500,null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    status=models.CharField(max_length=50,null=True,choices=STATUS)
